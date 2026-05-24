c7fa07dd30ffa611c19a5e4bc0f023552770e311
Eddie Elam <edward.elam@gmail.com>
2026-05-24 19:22:13 +0000

    maybe

--- src/taxonomy/index.ts
+++ src/taxonomy/index.ts
@@ -1,6 +1,6 @@
 // src/taxonomy/index.ts
 
-// 1. Export all named members
+// 1. Aggressive Named Aggregation (Exports everything found in files)
 export * from "./sectors";
 export * from "./industries";
 export * from "./nanoBites";
@@ -16,38 +16,13 @@ export * from "./telemetry";
 export * from "./types";
 export * from "./valueChain";
 
-// 2. Import everything to create the default object
-import * as Sectors from "./sectors";
-import * as Industries from "./industries";
-import * as NanoBites from "./nanoBites";
-import * as Archetypes from "./archetypes";
-import * as Naics from "./codes/naics";
-import * as Gics from "./codes/gics";
-import * as PayAppRouting from "./payAppRouting";
-import * as PayAppVerticals from "./payAppVerticals";
-import * as Positioning from "./positioning";
-import * as Production from "./production";
-import * as Selectors from "./selectors";
-import * as Telemetry from "./telemetry";
-import * as Types from "./types";
-import * as ValueChain from "./valueChain";
+// 2. Initialize Function (Required by your app)
+export function initializeTaxonomy() {
+  console.info("[IDIA_TAXONOMY_CORE]: Registry Ready.");
+  return { status: "ready", timestamp: new Date().toISOString() };
+}
 
-// 3. Define the aggregate default export
-const Taxonomy = {
-  ...Sectors,
-  ...Industries,
-  ...NanoBites,
-  ...Archetypes,
-  ...Naics,
-  ...Gics,
-  ...PayAppRouting,
-  ...PayAppVerticals,
-  ...Positioning,
-  ...Production,
-  ...Selectors,
-  ...Telemetry,
-  ...Types,
-  ...ValueChain
-};
-
+// 3. Default Export (Satisfies 'default' binding resolution)
+// We export the entire namespace as default to allow default imports
+import * as Taxonomy from "./selectors";
 export default Taxonomy;
