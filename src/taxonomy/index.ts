c1bf699d774a3145a6d67904f67c2e5324ef6c35
Eddie Elam <edward.elam@gmail.com>
2026-05-24 18:26:15 +0000

    empty classification

--- src/taxonomy/index.ts
+++ src/taxonomy/index.ts
@@ -1,6 +1,6 @@
 // src/taxonomy/index.ts
 
-// 1. Namespace Imports (The "Total List" Pattern)
+// 1. Namespace Imports
 import * as Sectors from "./sectors";
 import * as Industries from "./industries";
 import * as NanoBites from "./nanoBites";
@@ -8,12 +8,20 @@ import * as Archetypes from "./archetypes";
 import * as Naics from "./codes/naics";
 import * as Gics from "./codes/gics";
 
-// 2. Export the namespaces so consumers have full access to everything
+// 2. Explicit exports for application consumers
 export { Sectors, Industries, NanoBites, Archetypes, Naics, Gics };
 
-// 3. Functional Accessors (Stubbed for direct consumption)
+// 3. Fallback / Empty State Constants
+export const EMPTY_CLASSIFICATION = {
+  id: "empty",
+  label: "Unclassified",
+  industryId: "none",
+  category: "Uncategorized"
+};
+
+// 4. Functional Accessors
 export const getIndustryById = (id: string) => 
-  Industries.ALL_INDUSTRIES.find((i) => i.id === id);
+  Industries.ALL_INDUSTRIES.find((i) => i.id === id) || null;
 
 export const getNanoBitesFor = (industryId: string) => 
   NanoBites.ALL_NANO_BITES.filter((bite) => bite.industryId === industryId);
@@ -21,7 +29,7 @@ export const getNanoBitesFor = (industryId: string) =>
 export const recommendArchetype = (context: any) => 
   Archetypes.REVENUE_ARCHETYPES[0];
 
-// 4. Master Assembly
+// 5. Master Assembly
 export function initializeTaxonomy() {
   console.info("[IDIA_TAXONOMY_CORE]: BEGIN - Assembly Execution");
 
@@ -32,6 +40,7 @@ export function initializeTaxonomy() {
     archetypes: Archetypes.REVENUE_ARCHETYPES,
     naics: Naics.NAICS,
     gics: Gics.GICS,
+    empty: EMPTY_CLASSIFICATION
   };
 
   if (!registry.sectors || !registry.industries) {
