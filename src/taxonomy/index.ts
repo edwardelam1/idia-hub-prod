ceddcaf3675a295477008a3e6067241df5df3e8c
Eddie Elam <edward.elam@gmail.com>
2026-05-24 19:37:00 +0000

    omg

--- src/taxonomy/index.ts
+++ src/taxonomy/index.ts
@@ -1,47 +1,21 @@
 // src/taxonomy/index.ts
 
-// 1. Explicit Named Exports with Aliasing (Fixes the binding name mismatches)
+// 1. Explicit Named Exports only (No star exports)
+export { SECTORS } from "./sectors";
+export { ALL_INDUSTRIES, PRIMARY_INDUSTRIES, SECONDARY_INDUSTRIES, TERTIARY_INDUSTRIES, QUATERNARY_INDUSTRIES, QUINARY_INDUSTRIES } from "./industries";
+export { ALL_NANO_BITES } from "./nanoBites";
+export { REVENUE_ARCHETYPES } from "./archetypes";
 export { NAICS as NAICS_CODES } from "./codes/naics";
 export { GICS as GICS_CODES } from "./codes/gics";
+export { PRODUCTION_METHODS, breakEven } from "./production";
 
-// 2. Direct Re-exports for everything else
-export * from "./sectors";
-export * from "./industries";
-export * from "./nanoBites";
-export * from "./archetypes";
-export * from "./payAppRouting";
-export * from "./payAppVerticals";
-export * from "./positioning";
-export * from "./production";
+// 2. Types
+export type { NanoBite, IndustryNode, ArchetypeSpec, ProductionMethod } from "./types";
+
+// 3. Selectors and Utils
 export * from "./selectors";
-export * from "./telemetry";
-export * from "./types";
-export * from "./valueChain";
+export { initializeTaxonomy } from "./telemetry";
 
-// 3. Mandatory Assembly for Default Export
-import * as Sectors from "./sectors";
-import * as Industries from "./industries";
-import * as NanoBites from "./nanoBites";
-import * as Archetypes from "./archetypes";
-import * as Production from "./production";
-import * as Selectors from "./selectors";
-import { NAICS as NAICS_CODES } from "./codes/naics";
-import { GICS as GICS_CODES } from "./codes/gics";
-
-export function initializeTaxonomy() { 
-  return { status: "ready", timestamp: new Date().toISOString() }; 
-}
-
-const Taxonomy = {
-  ...Sectors,
-  ...Industries,
-  ...NanoBites,
-  ...Archetypes,
-  ...Production,
-  ...Selectors,
-  NAICS_CODES,
-  GICS_CODES,
-  initializeTaxonomy
-};
-
-export default Taxonomy;
+// STOP HERE. Do not add `export default`. 
+// If your components import Taxonomy from "@/taxonomy", change them 
+// to use named imports: import { ALL_NANO_BITES } from "@/taxonomy";
